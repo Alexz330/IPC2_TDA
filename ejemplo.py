55c4627633c6a0b75353449f80afcabcd3005b6e
Alexz330 <72354711+Alexz330@users.noreply.github.com>
2021-02-12 08:44:11 -0600

    Se agregó la listaDoble Enlazada

--- ejemplo.py
+++ ejemplo.py
@@ -42,3 +42,73 @@ class linked_list_circular:
 
       previous = node
       node = node.next # al nodo le asigno el siguiente nodo
+      
+ #lista Doblemente Enlazada    
+class cliente:
+  def __init__(self, nombre, no_habitacion):
+    self.nombre = nombre
+    self.no_habitacion = no_habitacion
+
+class node_de:
+  def __init__(self, cliente=None, next=None, previous=None):
+    self.cliente=cliente
+    self.previous= previous
+    self.next = next
+
+class linked_list_de:
+  
+  def __init__(self, head=None):
+    self.head = head
+    self.last = head
+    self.size = 0
+  
+  def insertar (self,cliente):
+    if self.size == 0:
+      self.head = node_de(cliente = cliente)
+      self.last = self.head
+    else:
+      new_node = node_de(cliente = cliente, next=self.head)
+      self.head.previous = new_node
+      self.head = new_node
+    self.size+=1 
+
+  def imprimir (self):
+    if self.head is None:
+      return
+    node = self.head
+    print(node.cliente.nombre, end = "=>")
+    while node.next:
+      node = node.next
+      print(node.cliente.nombre, end= "=>")
+  def eliminar(self, no_habitacion):
+    node = self.head
+    while node is not None:
+      if node.cliente.no_habitacion == no_habitacion:
+        if node.previous is not None:
+          if node.next:
+              node.previous.next = node.next
+              node.next.previous = node.previous
+          else:
+            node.previous.next = None
+            self.last = node.previous
+        else:
+          self.head = node.next 
+          node.next.previous = self.head
+          self.size -= 1
+          return True
+      else:
+        node = node.next
+        return False
+
+c1 = cliente("alexis lopez", 100)
+c2 = cliente("ana ramirez", 101)
+c3 = cliente("Sergi Roberto", 102)
+
+lista_de = linked_list_de()
+lista_de.insertar(c1)
+lista_de.insertar(c2)
+lista_de.insertar(c3)
+
+lista_de.imprimir()
+
+lista_de.eliminar(102)
